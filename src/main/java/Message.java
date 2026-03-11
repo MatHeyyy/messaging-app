525cc180a0cf450dc3f074be81a2a8fb6c1d7912
Matei Costinescu <mcostinescu213@gmail.com>
2026-03-11 12:48:33 +0000

    add Message class with properties and constructor

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,2 +1,16 @@
+import java.time.LocalDateTime;
+
+
 public class Message {
+    private String senderName;
+    private String content;
+    private LocalDateTime timestamp;
+    private boolean isRead;
+
+    public Message(String senderName, String content) {
+        this.senderName = senderName;
+        this.content = content;
+        this.timestamp = LocalDateTime.now();
+        this.isRead = false;
+    }
 }
