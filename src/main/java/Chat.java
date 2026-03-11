adaa12ede41ad814a6e65e835e5f284cfa8a45f1
Matei Costinescu <mcostinescu213@gmail.com>
2026-03-11 16:57:58 +0000

    add Chat class: implement participant name and message list with constructor

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Chat class represents a conversation between two users in the messaging system.
+ * It contains the name of the participant and a list of messages exchanged in the chat.
+ *
+ * @author Matei Costinescu
+ * @version 1.0
+ */
 public class Chat {
+    private String participantName;
+    private List<Message> messages;
+
+    /**
+     * Constructor for Chat class.
+     * @param participantName a String representing the name of the participant in the chat.
+     */
+    public Chat(String participantName) {
+        this.participantName = participantName;
+        this.messages = new ArrayList<>();
+    }
 }
